79beefb020cd08c97a6d01322112da12693ec0d7
Johannes Ahlmann <johannes.ahlmann@emc.com>
2015-12-14 17:09:40 +0000

    fixed prelude.imap, added zipWith

--- prelude.py
+++ prelude.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+"""
+Python functional tools inspired by Haskell Prelude
+"""
+
 import itertools as it
 from multiprocessing import Pool
 import operator
@@ -34,6 +38,9 @@ def nth(n, xs):
     return next(drop(n-1, xs))
 
 
+def last(xs):
+    return ft.reduce(lambda _,x: x, xs)
+
 def scan(f, it, state):
     """
     >>> list(scan(lambda x,y: x+y, [1,2,3], 0))
@@ -59,9 +66,10 @@ def iterate(f, x):
     [1, 2, 4, 8, 16]
     """
     s = x
+    yield s
     while True:
-        yield s
         s = f(s)
+        yield s
 
 
 def concat(xs):
@@ -80,44 +88,58 @@ def const(x):
     return lambda _: x
 
 
+def zipWith(f, *parts):
+    """
+    >>> list(zipWith(operator.add, [1,2,3], [4,5,6]))
+    [5, 7, 9]
+    """
+    return map(lambda x: f(*x), zip(*parts))
+
+
 """
 fold*, unfold
 sequence
 span
-groupBy
 List.tails, inits
 intersperse
 transpose
-takeWhile
-unzip, zipWith
 curry, uncurry
 """
+
 
 ###
 
 
-# TODO: Pool.imap would be infinitely more elegant,
-#   but unfortunately it is very broken ;(
+# source: http://stackoverflow.com/questions/24527006
+def chunks(iterable, size):
+    """
+    >>> list(list(c) for c in chunks(range(10), 3))
+    [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+    """
+    iterator = iter(iterable)
+    for first in iterator:
+        yield it.chain([first], it.islice(iterator, size - 1))
+
+
+# TODO: Pool.imap would be more elegant, but unfortunately it is very broken ;(
 def imap(func, iterable, chunksize=10000):
     """
-    >>> list(take(5, imap(ft.partial(operator.add, 1), range(5))))
+    >>> list(imap(ft.partial(operator.add, 1), range(5)))
     [1, 2, 3, 4, 5]
     """
     pool = Pool()
 
     # had trouble using it.chain with infinite iterable ;(
-    def chain(gss):
+    def chainss(gss):
         for gs in gss:
             for g in gs:
                 yield g
 
     def helper():
-        while True:
-            seg = it.islice(iterable, chunksize)
-            # TODO: abort loop when iterable is exhausted
-            yield pool.map(func, seg)
+        for chunk in chunks(iterable, chunksize): 
+            yield pool.map(func, chunk)
 
-    return chain(helper())
+    return chainss(helper())
 
 
 if __name__ == "__main__":
